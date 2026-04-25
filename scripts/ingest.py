cd0f1e8fb67ba343d93fc8d44a1dbcd13afacdc0
anpabeltj <patrapembinaan@gmail.com>
2026-04-25 22:47:58 +0800

    fix: enable airflow load_dotenv, data directory, and and chunksize parameter to avoid RAM loss when ingesting dataset

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 
 def get_db_engine():
-    # load_dotenv('/opt/airflow/.env')
-    load_dotenv()
+    load_dotenv('/opt/airflow/.env')
+    # load_dotenv()
     user = os.getenv("DB_USER")
     password = os.getenv("DB_PASS")
     host = os.getenv("DB_HOST")
@@ -21,12 +21,12 @@ def get_db_engine():
 def load_csv_to_postgres(file_path, engine):
     df = pd.read_csv(file_path)
     file_name = os.path.basename(file_path).replace(".csv", "")
-    df.to_sql(file_name, engine, if_exists='replace', index=False)
+    df.to_sql(file_name, engine, if_exists='replace', index=False, chunksize=1000)
     print(f"Loaded {file_name} into PostgreSQL")
 
 def ingest_all():
-    # data_dir = '/opt/airflow/data'
-    data_dir = '/Users/anpabelt/Downloads/ecommerce-elt-pipeline/data'
+    data_dir = '/opt/airflow/data'
+    # data_dir = '/Users/anpabelt/Downloads/ecommerce-elt-pipeline/data'
     engine = get_db_engine()
     list_file = os.listdir(data_dir)
     for file in list_file:
